dd7543b023ff5cf481ddd84c893434a89cdef8c8
Antoine Foggs <fireheartfoggs@gmail.com>
2018-10-14 13:44:12 -0500

    Added demo

--- testFiles/test.js
+++ testFiles/test.js
@@ -1,1 +1,15 @@
-//skeleton for test cases
+  if(testcase == 0) {
+    $.ajax({
+      type: "POST",
+      url: https://api4.apidaze.io/4dd2c918/sms/send?api_secret=02acb1c22bf274223bfbd861da334d59,
+      data: {
+            from: 13129850406,
+            to: 12244328255,
+            body: "This is a test of ajax"
+            },
+      success: success,
+      dataType: string
+    });
+   });
+ }
+})(jQuery);
